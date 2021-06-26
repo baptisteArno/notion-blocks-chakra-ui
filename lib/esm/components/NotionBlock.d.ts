6f730e356a2f41500142b35b3daade1b021fced2
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2021-06-26 17:47:43 +0200

    Forgot to build

--- lib/esm/components/NotionBlock.d.ts
+++ lib/esm/components/NotionBlock.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="react" />
+import { Image } from '@chakra-ui/react';
 import { Block } from '@notionhq/client/build/src/api-types';
-export declare const NotionBlock: ({ block }: {
+export declare const NotionBlock: ({ block, customImage, }: {
     block: Block;
+    customImage?: {
+        Image: (props: any) => JSX.Element;
+        props: any;
+    } | undefined;
 }) => JSX.Element;
